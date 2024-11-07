2ee68d2b54c3fc4380b90042b0c88d893912bcf3
Eric Kapilik <eric.kapilik@sanctuary.ai>
2024-11-06 20:39:44 -0800

    basic macroquad integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const DEV_MODE: bool = true;
+
+#[macroquad::main("Asteroids")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        if DEV_MODE {
+            draw_fps();
+        }
+        next_frame().await // Blocks until the next frame, used to control the game loop.
+    }
+}
+
+fn draw_fps() {
+    draw_text(&format!("FPS: {:.2}", get_fps()), 10.0, 20.0, 20.0, WHITE);
 }
