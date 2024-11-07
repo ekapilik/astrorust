a6d8efb5b79a7625a0db35040e63a2cf8fd1b29c
Eric Kapilik <eric.kapilik@sanctuary.ai>
2024-11-06 21:19:25 -0800

    feat: basic main menu

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use macroquad::prelude::*;
+mod game_states;
+
+use macroquad::{prelude::*, telemetry::frame};
 
 const DEV_MODE: bool = true;
+const TARGET_FPS: f32 = 60.0;
 
 #[macroquad::main("Asteroids")]
 async fn main() {
+    let mut game_state = game_states::GameState::MainMenu;
+    let frame_sleep_duration = std::time::Duration::from_secs_f32(1.0 / TARGET_FPS);
+
     loop {
         clear_background(BLACK);
         if DEV_MODE {
             draw_fps();
         }
+
+        game_states::update_game_state(&mut game_state);
+        game_states::render(&game_state);
+
+        std::thread::sleep(frame_sleep_duration);
         next_frame().await // Blocks until the next frame, used to control the game loop.
     }
 }
